2df75a853ed3739a3fcd5180df8ac53a9daeb76b
Denniskwg <dkamau476@gmail.com>
2023-07-11 13:03:40 +0300

    corrections

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -26,13 +26,15 @@ def canUnlockAll(boxes):
         return indexes
 
     index_list = get_values(boxes[0])
+    print(index_list)
 
     def compare_list(lst):
         reference = []
+        sorted_lst = lst.sort()
         for i in range(len(boxes)):
             reference.append(i)
 
-        if reference == lst:
+        if reference == sorted_lst:
             return True
         else:
             return False
