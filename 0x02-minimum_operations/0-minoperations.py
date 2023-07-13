4b698c511703245799fa227ec69c5f282e1e1cce
Denniskwg <dkamau476@gmail.com>
2023-07-14 00:36:30 +0300

    adding solution for minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""0-minoperations defines a function minOperations
+that takes one integer argument
+"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations
+    needed to result in exactly n H characters in a file
+    the available operations are copy all and paste
+    """
+    if n == 0:
+        return 0
+
+    if n % 6 == 0:
+        return 5 + minOperations(n / 6)
+    elif n % 3 == 0:
+        return 3 + minOperations(n / 3)
+    elif n % 2 == 0:
+        return 2 + minOperations(n / 2)
+    else:
+        return 0
