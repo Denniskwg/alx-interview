4a5e3560b5f7806be4addacdd0accbff1b998501
Denniskwg <dkamau476@gmail.com>
2023-07-27 14:07:41 +0300

    adding solution for utf-8 encoding

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    """checks if a given data set represents a valid
+    utf-8 encoding. Reaturns true if data is valid utf-8
+    encoding else false
+    """
+    try:
+        byte_data = bytes(data)
+        decoded_string = byte_data.decode('utf-8')
+        return True
+    except (UnicodeDecodeError, ValueError):
+        return False
