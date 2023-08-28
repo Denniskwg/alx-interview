66c9f04e60e27637de5e1d6a860d288a81f78910
Denniskwg <dkamau476@gmail.com>
2023-08-28 10:35:00 +0300

    corrections

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,7 @@
 """
 
 
-def makeChange(coins, total, idx=0):
+def makeChange(coins, total, idx=0, num=0):
     """Given a pile of coins of different values
     determine the fewest number of coins needed
     to meet a given amount total
@@ -12,13 +12,14 @@ def makeChange(coins, total, idx=0):
         return 0
     if len(coins) == 0:
         return -1
-    sorted_list = sorted(coins, reverse=True)
+    lst = sorted(coins, reverse=True)
 
-    if idx == len(sorted_list) and (total - sorted_list[idx - 1]) < 0:
-        return -1 * (idx + 1)
-    if total < sorted_list[len(sorted_list) - 1]:
+    if idx == len(lst) and (total - lst[-1]) < 0:
+        return -1 * (num + 1)
+    if total < lst[-1]:
         return -1
-    largest = sorted_list[idx]
+    largest = lst[idx]
     remainder = total - (largest * int(total / largest))
-    steps = int(total / largest) + makeChange(sorted_list, remainder, idx + 1)
+    new_num = num + int(total / largest)
+    steps = int(total / largest) + makeChange(lst, remainder, idx + 1, new_num)
     return steps
