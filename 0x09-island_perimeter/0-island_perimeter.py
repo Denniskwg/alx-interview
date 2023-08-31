22ae0e3b1e353f4af1ee0509f838eabbdd408697
Denniskwg <dkamau476@gmail.com>
2023-08-31 23:51:40 +0300

    More corrections

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,11 +12,24 @@ def island_perimeter(grid):
     Each cell is square, with a side length of 1
     Cells are connected horizontally/vertically
     """
+    total_sum = 0
+
+    for inner_list in grid:
+        for num in inner_list:
+            total_sum += num
+    if total_sum == 1:
+        return 4
     total = 0
+    flag = False
     for i in range(len(grid)):
-        if i > 0 and sum(grid[i]) == 0:
-            total = 0
-            break
+        if i > 0:
+            k = i - 1
+            m = i - 2
+            if k > 0 and m > 0:
+                if sum(grid[k]) == 0 and sum(grid[m]) > 0:
+                    if sum(grid[i]) > 0:
+                        total = 0
+                        break
         for j in range(len(grid[i])):
             num = 0
             if grid[i][j] == 1:
@@ -28,8 +41,18 @@ def island_perimeter(grid):
                     num = num + 1
                 if i < (len(grid) - 1) and grid[i + 1][j] == 0:
                     num = num + 1
-                if num == 4 and total > 0:
-                    num = 0
+                if j == (len(grid[i]) - 1):
+                    print("yes")
+                    num = num + 1
+                if j == 0:
+                    num = num + 1
+                if num == 4:
+                    print("yes2")
+                    total = 0
+                    flag = True
+                    break
                 total = total + num
+        if flag:
+            break
 
     return total
